0465e28f4bb4e3394238d389c4b4d18352dc5a89
Derek Miranda <derek.miranda@ff0000.com>
2019-07-02 11:55:39 -0700

    cleans: removes TT images
    - repub

--- dist/1-build/common/js/EndFrame/postmarkups/index.js
+++ dist/1-build/common/js/EndFrame/postmarkups/index.js
@@ -1,51 +1,58 @@
-import { createSideBySidePostMarkups } from './horizontalSideBySide.js'
-import horizontalStacked from './horizontalStacked.js'
-import { Align } from 'ad-view'
-import { ObjectUtils } from 'ad-utils'
+import { createSideBySidePostMarkups } from "./horizontalSideBySide.js";
+import horizontalStacked from "./horizontalStacked.js";
+import { Align } from "ad-view";
+import { ObjectUtils } from "ad-utils";
 
 const {
-	leftPostMarkup,
-	offCenterLeftPostMarkup,
-	centerPostMarkup,
-	offCenterRightPostMarkup,
-	rightPostMarkup
+  leftPostMarkup,
+  offCenterLeftPostMarkup,
+  centerPostMarkup,
+  offCenterRightPostMarkup,
+  rightPostMarkup
 } = createSideBySidePostMarkups({
-	defaultArgs: {
-		ctaLogoOffset: 20,
-		tuneInFontSize: 18,
-		tuneInLockupOffset: 16
-	},
-	yAlign: {
-		type: Align.BOTTOM,
-		offset: -26
-	},
-	layoutXAligns: {
-		leftXAlign: {
-			type: Align.LEFT,
-			against: 65
-		},
-		offCenterLeftXAlign: {
-			type: Align.LEFT,
-			against: 207
-		},
-		centerXAlign: Align.CENTER,
-		offCenterRightXAlign: {
-			type: Align.RIGHT,
-			offset: -207
-		},
-		rightXAlign: {
-			type: Align.RIGHT,
-			offset: -65
-		}
-	}
-})
+  defaultArgs: {
+    ctaLogoOffset: 20,
+    tuneInFontSize: 18,
+    tuneInLockupOffset: 16
+  },
+  yAlign: {
+    type: Align.BOTTOM,
+    offset: -26
+  },
+  layoutXAligns: {
+    leftXAlign: {
+      type: Align.LEFT,
+      against: 65
+    },
+    offCenterLeftXAlign: {
+      type: Align.LEFT,
+      against: 207
+    },
+    centerXAlign: Align.CENTER,
+    offCenterRightXAlign: {
+      type: Align.RIGHT,
+      offset: -207
+    },
+    rightXAlign: {
+      type: Align.RIGHT,
+      offset: -65
+    }
+  }
+});
 
 function stackedPostMarkup() {
-	horizontalStacked({
-		brandingLockupOffset: 16,
-		brandingLockupRightPadding: 25,
-		tuneInFontSize: 18
-	})
+  horizontalStacked({
+    brandingLockupOffset: 16,
+    brandingLockupRightPadding: 25,
+    tuneInFontSize: 18
+  });
 }
 
-export { leftPostMarkup, offCenterLeftPostMarkup, centerPostMarkup, offCenterRightPostMarkup, rightPostMarkup, stackedPostMarkup }
+export {
+  leftPostMarkup,
+  offCenterLeftPostMarkup,
+  centerPostMarkup,
+  offCenterRightPostMarkup,
+  rightPostMarkup,
+  stackedPostMarkup
+};
